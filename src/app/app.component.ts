ec4e109de23cd91b5049b996bf7d6b25546e7ce5
Deepak Mane <deepak.mane123@gmail.com>
2019-06-25 03:43:40 -0700

    184 - RD:Creating the Form and Registering the Control1s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: ElementRef) {
+    console.log(form);
   }
 }
